1886d54a8f0ce994768ca35af5f6026c58b97d29
mevans <mv.evans.phd@gmail.com>
2026-03-04 13:25:52 +0200

    attempting a hierarchical init file again

--- src/pivot_dhis_tools/__init__.py
+++ src/pivot_dhis_tools/__init__.py
@@ -4,4 +4,5 @@ from . import get_dataElements
 from . import post_dataElements
 from . import launch_analytics
 
-from . import pridec
+from .pridec.check_dhis_value import check_dhis_value
+from .pridec.update_dataStoreKey import update_dataStoreKey
