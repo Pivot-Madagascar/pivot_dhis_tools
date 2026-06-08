aab119083884fc0490bb378b904ee0bfe0c4154d
mevans <mv.evans.phd@gmail.com>
2026-06-08 16:14:40 +0200

    seperate clear and launch analytics #2

--- labNotebook-dhis_tools.md
+++ labNotebook-dhis_tools.md
@@ -4,7 +4,7 @@
 ## Useful Things
 
 `source .venv/bin/activate`: to start the python venv
-`pip  
+`pip  install -e .`
 
 for testing on d2 docker-test: 
 ```
@@ -12,7 +12,26 @@ DHIS2_PRIDEC_URL="http://localhost:8082/"
 DHIS2_TOKEN="d2pat_odhYW86O8auDuQ73u4r3HElEJxMFQziM3326734980"
 ```
 
-for testing on DHIS2 play instance:
+for updating releases (from main branch):
+
+```
+# update version in toml
+git add pyproject.toml
+git commit -m "update version to X.X.X"
+git push
+
+git tag vX.X.X
+git push origin vX.X.X
+#manually add relase on github
+```
+
+## 2026-06-08
+
+I had updated the package to clear the analytics table before it gets built and it was causing errors. Now have moved to its own function and added a raise for status check.
+
+## 2026-05-11
+
+Created a built version of package and updated relase to v0.1.0 on github.
 
 ## 2026-5-08
 
--- src/pivot_dhis_tools/clear_analytics.py
+++ src/pivot_dhis_tools/clear_analytics.py
@@ -4,12 +4,12 @@ from requests.auth import HTTPBasicAuth
 import logging
 logger = logging.getLogger(__name__)
 
-def launch_analytics(dhis_url: str,
+def clear_analytics(dhis_url: str,
                      user: str | None = None, 
                      pwd: str | None = None, 
                      token: str | None = None):
     """
-    Rebuilds Analytics Tables on a DHIS2 instance by clearing the cache and then rebuilding the tables
+    Clears Analytics table cache
 
     Args:
         dhis_url: URL of the DHIS2 instance.
@@ -28,37 +28,19 @@ def launch_analytics(dhis_url: str,
     headers = {'Authorization': f'ApiToken {token}'} if token else {}
     auth = None if token else HTTPBasicAuth(user, pwd)
 
-    # clear_cache_endpoint = "/api/maintenance/analyticsTablesClear"
+    clear_cache_endpoint = "/api/maintenance/analyticsTablesClear"
 
-    # url = f"{dhis_url.rstrip('/')}{clear_cache_endpoint}"
+    url = f"{dhis_url.rstrip('/')}{clear_cache_endpoint}"
 
-    #     #send request
-    # response = requests.post(url, headers=headers, auth=auth)
-
-    # resp_text = response.json().get("response")
-    # if response.ok:
-    #     logger.info("Analytics tables dropped.")
-    #     logger.debug("Response: %s", resp_text)
-    # else: 
-    #     logger.error("Error clearing analytics tables with response: %s", resp_text)
-
-    analytics_endpoint = "/api/33/resourceTables/analytics"
-
-
-    url = f"{dhis_url.rstrip('/')}{analytics_endpoint}"
-    
-    #send request
+        #send request
     response = requests.post(url, headers=headers, auth=auth)
     response.raise_for_status()
 
     resp_text = response.json().get("response")
-
     if response.ok:
-        analytics_endpoint = response.json().get("response")['relativeNotifierEndpoint']
-        logger.info("Successfully launched export of analytics table.")
-        logger.info("View status at %s", f"{dhis_url.rstrip('/')}{analytics_endpoint}")
+        logger.info("Analytics tables dropped.")
         logger.debug("Response: %s", resp_text)
-    else:
-        logger.error("Analytics table build failed to start with response: %s", resp_text)
-    
+    else: 
+        logger.error("Error clearing analytics tables with response: %s", resp_text)
+
     return response
